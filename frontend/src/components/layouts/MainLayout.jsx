73471e48aabb1f89cd8d1d81ce396ef750a49607
Nguyên Khôi <115073243+SuKay712@users.noreply.github.com>
2024-10-07 21:16:47 +0700

    [FE][Guest][Auth] Register page

--- frontend/src/components/layouts/MainLayout.jsx
+++ frontend/src/components/layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
 import Footer from "../footers";
 import Header from "../headers";
 import React from "react";
+import { Outlet } from 'react-router-dom';
 import "./MainLayout.scss";
 
 function MainLayout(props) {
