c26f3ce12b5e0d3cb586ecefd1c322343cd866c4
DungNguyen1702 <vandung17022003@gmail.com>
2024-10-24 21:02:17 +0700

    [BE][Staff, Admin][Order management] API update thông tin của order theo OrderID

--- backend/src/modules/order/admin.order.controller.ts
+++ backend/src/modules/order/admin.order.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { AdminOrderService } from './admin.order.service';
 import { AuthGuard } from '../../common/guards/auth.guard';
 import { RoleGuard } from '../../common/guards/role.guard';
@@ -7,13 +7,19 @@ import { AdminOrdersRequest } from './dtos/adminOrderRequest';
 
 @Controller('/admin/orders')
 export class AdminOrderController {
-    constructor(private readonly adminOrderSerview : AdminOrderService){}
+    constructor(private readonly adminOrderSerview: AdminOrderService) {}
 
-    
     @Get()
     @UseGuards(new RoleGuard([AccountRoleEnum.ADMIN, AccountRoleEnum.STAFF]))
     @UseGuards(AuthGuard)
-    getListOrder(@Query() query : AdminOrdersRequest, @Lang() lang : string){
+    getListOrder(@Query() query: AdminOrdersRequest, @Lang() lang: string) {
         return this.adminOrderSerview.getListOrder(query, lang);
+    }
+
+    @Get('/:id')
+    @UseGuards(new RoleGuard([AccountRoleEnum.ADMIN, AccountRoleEnum.STAFF]))
+    @UseGuards(AuthGuard)
+    getOrderDetail(@Param('id') id: number, @Lang()lang : string) {
+        return this.adminOrderSerview.getOrderDetail(id, lang);
     }
 }
